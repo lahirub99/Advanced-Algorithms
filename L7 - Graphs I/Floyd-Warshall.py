8ea1f6d29a1751bcce72a18dbf8e5973f15bdfad
Lahiru Bandara <lahirub@ceyentra.com>
2023-09-24 23:51:02 +0530

    Floyd Warshall - Initialization

---
+++ L7 - Graphs I/Floyd-Warshall.py
@@ -0,0 +1,31 @@
+"""
+Basic Idea of the Approach 
+• Iterate n times over matrix D, using index k 
+• (k-1)th iteration: 
+    D matrix has the solution where paths only contain vertices 1 to k-1 
+• Next, in kth iteration: 
+    compare the costs of 
+    1.going from i to j using only vertices 1,…,k-1 (stored at dij in the (k-1)th iteration) 
+    2.using the kth vertex as an intermediate step, which is dik (go from i to k) plus dkj (go from k to j)
+    –Then remember the lower cost path
+"""
+
+
+def FloydWarshall(graph):
+    # Initialize matrix with zeros
+    D = [[0 for i in range(len(graph))] for j in range(len(graph))]
+
+
+
+
+### Test 01:
+INFINITY, V = 99999, 4
+# V = Number of vertices in the graph
+
+graph = [[0, 5, INFINITY, 10], 
+         [INFINITY, 0, 3, INFINITY],
+         [INFINITY, INFINITY, 0, 1],
+         [INFINITY, INFINITY, INFINITY, 0]]
+
+
+print(FloydWarshall(graph))
