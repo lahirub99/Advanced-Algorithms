1e0c9b42670b2349d78177149e6a1a93ff16e222
Lahiru Bandara <lahirub@ceyentra.com>
2023-10-22 21:45:18 +0530

    Divide and Conquer Analysis

--- L11 - Multi Threaded Algorithms 2/Divide_and_conquer.py
+++ L11 - Multi Threaded Algorithms 2/Divide_and_conquer.py
@@ -28,6 +28,8 @@ def divide_and_conquer(A, B, n):
     # perform normal matrix multiplication and return
     if n == 1:
         C[0][0] = A[0][0] * B[0][0]
+        #### T(1) = O(1)
+
     else: 
         # Divide A, B, and C into n/2 x n/2 submatrices
         A11, A12, A21, A22 = devide_matrix(A, n)
@@ -39,7 +41,9 @@ def divide_and_conquer(A, B, n):
         C12 = add_matrices(divide_and_conquer(A11, B12, n//2), divide_and_conquer(A12, B22, n//2), n//2)
         C21 = add_matrices(divide_and_conquer(A21, B11, n//2), divide_and_conquer(A22, B21, n//2), n//2)
         C22 = add_matrices(divide_and_conquer(A21, B12, n//2), divide_and_conquer(A22, B22, n//2), n//2)
-
+        #### Recursively computing the 8 submatrices takes 8T(n/2) time.
+        #### Where T(n) is the time complexity of the algorithm. It has to conquer 8 subproblems of size n/2.
+        
         # Combine the 8 submatrices into a single C matrix
         for i in range(n//2):
             for j in range(n//2):
@@ -47,5 +51,23 @@ def divide_and_conquer(A, B, n):
                 C[i][n//2+j] = C12[i][j]
                 C[n//2+i][j] = C21[i][j]
                 C[n//2+i][n//2+j] = C22[i][j]
-        
+        #### Comnining takes O(n^2) time.
+        #### T(n) = 8T(n/2) + O(n^2)
+
     return C
+
+    #### Using master method, we can conclude that the time complexity of the algorithm is O(n^3).
+    #### T(n) = aT(n/b) + f(n)
+    ####    where a = 8, b = 2, f(n) = O(n^2)
+    #### T(n) = 8T(n/2) + O(n^2)
+    #### 
+    #### f(n) = O(n^2) = O(n^log2(8))
+    ####    logb(a) = log2(8) 
+    ####            = 3
+    #### T(n) = O(n^3)
+    
+    #### where a = 8 (since there are 8 subproblems), b = 2 (since the subproblems are half the size of the original problem), and f(n) = O(n^2) (since the work done outside of the recursive calls is O(n^2)).
+    # Using the master method, we can conclude that the time complexity of the algorithm is O(n^3). This is because:
+    # logb(a) = log2(8) = 3
+    # f(n) = O(n^2) = O(n^log2(8))
+    # Since logb(a) = 3 > log2(2) = 1, we have case 3 of the master method, which gives us a time complexity of O(n^log2(8)) = O(n^3).
